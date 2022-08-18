79d25966fe27e7818d6975915b391585d9271679
thiago morgado <thiago_morgado@hotmail.com>
2022-08-18 01:56:25 -0300

    ...

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import app from './app';
+
 require("dotenv/config");
-const PORT = process.env.PORT || 3000;
+
+export const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Listening on port 5000'+ PORT);
+  console.log('Listening on port 5000' + PORT);
 })
