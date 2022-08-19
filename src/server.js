21326324718d5fc037bf8dccafb2d604ee81192f
thiago morgado <thiago_morgado@hotmail.com>
2022-08-18 21:50:58 -0300

    subindo para heroku

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ require("dotenv/config");
 
 export const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('Listening on port 5000' + PORT);
+app.listen(5000, () => {
+  console.log('Listening on port 5000 /' + PORT);
 })
